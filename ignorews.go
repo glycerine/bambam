c0401085efd9ac4b0c7b2c3cc8784fa7a607a26a
Jason E. Aten <j.e.aten@gmail.com>
2014-11-10 03:29:11 -0800

    ft. failing tests in place for embeds and ignoring lowercase go struct fields

---
+++ ignorews.go
@@ -0,0 +1,63 @@
+package main
+
+import "fmt"
+
+// compare strings, ignoring spaces
+
+const debugEIS = false
+
+func equalIgnoringSpaces(a, b string) (res bool) {
+
+	defer func() {
+		if debugEIS {
+			if res {
+				fmt.Printf("\n\n equalIgnoringSpaces says these are the same: '%s' and '%s'\n", a, b)
+			} else {
+				fmt.Printf("\n\n equalIgnoringSpaces says these are NOT the same: '%s' and '%s'\n", a, b)
+			}
+		}
+	}()
+
+	r := []rune(a)
+	s := []rune(b)
+	nextr := 0
+	nexts := 0
+
+	for {
+		// skip past spaces in both r and s
+		for nextr < len(r) {
+			if r[nextr] == ' ' {
+				nextr++
+			} else {
+				break
+			}
+		}
+
+		for nexts < len(s) {
+			if s[nexts] == ' ' {
+				nexts++
+			} else {
+				break
+			}
+		}
+
+		if nextr >= len(r) && nexts >= len(s) {
+			return true
+		}
+
+		if nextr >= len(r) {
+			return false
+		}
+		if nexts >= len(s) {
+			return false
+		}
+
+		if r[nextr] != s[nexts] {
+			return false
+		}
+		nextr++
+		nexts++
+	}
+
+	return false
+}
