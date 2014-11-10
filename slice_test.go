cbe8986e8eb42241bae964d1412537291fe026eb
Jason E. Aten <j.e.aten@gmail.com>
2014-11-10 00:49:21 +0000

    ft. failing test for slice -> List() conversion.

---
+++ slice_test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"testing"
+
+	cv "github.com/smartystreets/goconvey/convey"
+)
+
+func TestSliceToList(t *testing.T) {
+
+	cv.Convey("Given a parsable golang source file with struct containing a slice", t, func() {
+		cv.Convey("then the slice should be converted to a List() in the capnp output", func() {
+
+			ex0 := `
+type s1 struct {
+  MyInts []int
+}`
+			cv.So(ExtractString2String(ex0), cv.ShouldEqual, `struct S1 { myInts @0: List(Int64); } `)
+
+		})
+	})
+}
